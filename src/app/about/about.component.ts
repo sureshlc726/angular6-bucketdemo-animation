f9a76bf3e702c8eda9231b8e32f80233c035baa5
Ribera Solutions <riberasolutions@Riberas-MacBook-Pro-2.local>
2018-06-13 17:32:12 +0545

    Initial commit

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss']
+})
+export class AboutComponent implements OnInit {
+
+  constructor(private router: Router, private route: ActivatedRoute) { 
+  	this.route.params.subscribe(res => console.log(res.id));
+  }
+
+  ngOnInit() {
+  }
+
+  sendMeHome() {
+  	this.router.navigate(['']);
+  }
+
+}
---
+++ src/app/data.service.ts
@@ -0,0 +1,15 @@
+// import { Injectable } from '@angular/core';
+// // import {BehaviorSubject} from 'rxjs/BehaviorSubject';
+
+// @Injectable()
+// export class DataService {
+
+//   private goals = new BehaviorSubject<any>(["My first goal.", "My second goal.", "My third goal."]);
+//   goal = this.goals.asObservable();	
+  
+//   constructor() { }
+
+//   changeGoal(goal) {
+//   	this.goals.next(goal);
+//   }
+// }
